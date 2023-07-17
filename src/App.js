55f8a2af6b4bfd8a70ddb0a16c8f84bbae167894
Ishwor Basyal <basyalishu123@gmail.com>
2023-07-17 13:58:10 +0545

    React redux counter app project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./Store";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-green-300 h-screen w-full px-4 py-8">
+        <h1 className="text-black font-bold text-4xl text-center ">
+          Counter App
+        </h1>
+        <h2 className="text-center font-medium text-2xl mt-8">
+          Result: {counter}
+        </h2>
+        <div className="flex justify-center py-2">
+          <button
+            className="border bg-white rounded m-1 w-[100px]  p-1 hover:text-red-600 "
+            onClick={increment}
+          >
+            Increment
+          </button>
+          <button
+            className="border bg-white rounded m-1 w-[100px]  p-1 hover:text-red-600 "
+            onClick={decrement}
+          >
+            Decrement
+          </button>
+          <button
+            className="border bg-white rounded m-1 w-[100px]  p-1 hover:text-red-600 "
+            onClick={addBy}
+          >
+            Add By 10
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
